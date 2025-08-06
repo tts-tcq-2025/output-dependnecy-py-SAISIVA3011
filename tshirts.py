760b75ee9a5b6db591f756e21f585c550f5b6e81
SAISIVA3011 <kvnsiva2001@gmail.com>
2025-08-06 23:29:54 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -6,9 +6,12 @@ def size(cms):
         return 'M'
     else:
         return 'L'
-
-
+ 
+ 
 assert(size(37) == 'S')
+assert(size(38) == 'M')
 assert(size(40) == 'M')
+assert(size(42) == 'L')
 assert(size(43) == 'L')
-print("All is well (maybe!)")
+print("All is well...")
+
